a2a42e48e5eef80db4dba8f7799bcf76e822233d
Teo <teo.bostiog@yahoo.com>
2024-09-18 23:39:31 +0300

    setup Net Ninja course

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Create.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+
+export default function Create() {
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom="true"
+      >
+        Create a New Note
+      </Typography>
+    </div>
+  );
+}
